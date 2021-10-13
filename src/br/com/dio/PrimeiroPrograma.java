2b250ae51e39af67995c5df437c5cbee8502d989
GildemarNascimento <gildemargg@hotmail.com>
2021-10-13 15:06:53 -0300

    Acrescenta a classe livro.

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -10,5 +10,44 @@ public class PrimeiroPrograma {
         gato.setCor("Preto");
         gato.setIdade(2);
             System.out.println(gato);
+
+        Livro livro = new Livro("O problema dos tres corpos", "300");
+
+        System.out.println(livro);
+
+    }
+
+    static class Livro{
+        private String nome;
+        private String numPaginas;
+
+        public Livro(String nome, String numPaginas) {
+            this.nome = nome;
+            this.numPaginas = numPaginas;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public String getNumPaginas() {
+            return numPaginas;
+        }
+
+        public void setNumPaginas(String numPaginas) {
+            this.numPaginas = numPaginas;
+        }
+
+        @Override
+        public String toString() {
+            return "Livro{" +
+                    "nome='" + nome + '\'' +
+                    ", numPaginas='" + numPaginas + '\'' +
+                    '}';
+        }
     }
 }
